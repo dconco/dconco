36731ba9ee6703d00352d33f40565fca896e340c
dconco <onipededavid208@gmail.com>
2026-08-12 20:45:34 +0100

    refractor fontsource style from index.css to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import { portfolioTheme } from './theme/portfolioTheme'
 import { CartProvider } from './contexts/CartContext'
 import App from './App'
 import 'aos/dist/aos.css'
+import "@fontsource/syne/400.css"
+import "@fontsource/syne/700.css"
+import "@fontsource/syne/800.css"
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/500.css"
+import "@fontsource/inter/600.css"
+import "@fontsource/jetbrains-mono/400.css"
+import "@fontsource/jetbrains-mono/600.css"
+import "@fontsource/noto-serif"
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
